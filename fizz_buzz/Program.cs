5e8aab4a98852a41cf03f6023228f7c2a559d8a5
Manikandan R <rmanikandan102000@gmail.com>
2023-06-16 11:19:38 +0530

    initial commit

---
+++ fizz_buzz/Program.cs
@@ -0,0 +1,44 @@
+﻿// fizz buzz
+
+class FizzBuzz
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter starting number: ");
+        int startNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter ending number: ");
+        int endNum = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter divisor 1: ");
+        int divisor1 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter divisor 2: ");
+        int divisor2 = Convert.ToInt32(Console.ReadLine());
+
+        string[] values = { "Fizz", "Buzz", "FizzBuzz" };
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            if (i % divisor1 == 0 && i % divisor2 == 0)
+            {
+                Console.WriteLine(values[2]);
+            }
+            else if (i % divisor1 == 0)
+            {
+                Console.WriteLine(values[0]);
+            }
+            else if (i % divisor2 == 0)
+            {
+                Console.WriteLine(values[1]);
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
+
+
---
+++ word_count/Program.cs
@@ -0,0 +1,20 @@
+﻿// word and letters count in a sentance
+
+class Word_count
+{
+    static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        string[] words = word.Split();
+        int total_count = 0;
+
+        int count = words.Length;
+        Console.WriteLine("words in the sentance: " + count);
+
+        foreach(string w in words)
+        {
+            total_count += w.Length;
+        }
+        Console.WriteLine("total letters in the sentance: " + total_count);
+    }
+}
